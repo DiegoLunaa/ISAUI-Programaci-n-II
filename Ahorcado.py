952342c3542178c8aaa9868d92573d73d1d70691
Diego Gabriel Luna <109829485+DiegoLunaa@users.noreply.github.com>
2024-05-09 20:39:03 -0300

    Update Ahorcado.py
    Lo comenté y eliminé una parte del código que no hacia falta.

--- Ahorcado.py
+++ Ahorcado.py
@@ -4,23 +4,24 @@ caracteres_aceptados = "abcdefghijklmnñopqrstuvwxyz"
 
 palabras = ['pelota', 'cancha', 'partido', 'gol']
 
+# Elijo una palabra al azar de la lista ya predefinida
 palabra_random = random.choice(palabras)
 
+# Oculto la palabra
 palabra_oculta = ["_" for _ in palabra_random]
-
-longitud_palabra = len(palabra_random)
 
 intentos = 7
 
 input("¡Juego del ahorcado!\nREGLAS Y CONDICIONES...\n- Tendrás 7 intentos, cada error te descontará 1.\n- Deberás escribir una letra por intento y no la puedes repetir.\n- Podrás escribir una palabra pero si no aciertas perderás.\nPresiona Enter para comenzar... ")
 
 while True:
+    # Muestro la cantidad de letras ocultas y la separo en espacios 
     print("\nPalabra: ", " ".join(palabra_oculta))
-
     print("Intentos restantes: ", intentos)
 
     letra_ingresada = input("Ingresar letra elegida: ").lower()
 
+    # Opción por si el jugador elige una palabra 
     if len(letra_ingresada) > 1:
         
         if letra_ingresada == palabra_random:
@@ -31,14 +32,14 @@ while True:
             break
 
     
-
+    # Aviso al jugador de qué el elemento ingresado no es válido
     if letra_ingresada not in caracteres_aceptados:
         print("\n---------¡¡¡Has ingresado un caracter no aceptado. Recuerda que solo se aceptan letras!!!---------\n---------¡¡¡Has ingresado un caracter no aceptado. Recuerda que solo se aceptan letras!!!---------")
         continue
-
+    
+    # Si la letra es correcta se le avisa y se muestra, si no pierde un intento
     elif letra_ingresada in palabra_random:
         print("La letra es correcta")
-        longitud_palabra -= 1
         for i in range(len(palabra_random)):
             if palabra_random[i] == letra_ingresada:
                 palabra_oculta[i] = letra_ingresada
@@ -46,16 +47,12 @@ while True:
     else:
         print("La letra es incorrecta")
         intentos -= 1
-
+    # Cuando ya no hay palabras ocultas termina el juego
     if "_" not in palabra_oculta:
-        print("\n¡Felicidades! Has adivinado la palabra: ", palabra_random.capitalize())
+        print("\n¡Felicitaciones! Has adivinado la palabra.\nLa palabra oculta era: ", palabra_random.capitalize())
         break
 
     if intentos == 0:
         print("GAME OVER\nTu número de intentos es 0.")
         break
-    
-
-
-    
 
